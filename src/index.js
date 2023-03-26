8955e93a3852b8599eeb7a77e4b8f1b40deef5cf
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-03-27 00:02:23 +0200

    second commit

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./rtk/Store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Provider store={store}></Provider>
       <App />
     </BrowserRouter>
   </React.StrictMode>
